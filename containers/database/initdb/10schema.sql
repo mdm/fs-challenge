9399f94aa552c1f689792b9ada4a4c4132c545e7
Marc Dominik Migge <marcmigge@gmx.net>
2017-10-08 17:29:07 +0200

    Setup and seed MariaDB using Docker

---
+++ containers/database/initdb/10schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE data (
+    id INTEGER AUTO_INCREMENT,
+    city VARCHAR(255),
+    start_date DATE,
+    end_date DATE,
+    price INTEGER,
+    status VARCHAR(255),
+    color VARCHAR(7),
+    PRIMARY KEY (id)
+);
