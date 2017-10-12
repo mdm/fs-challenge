03e991755155e199462e92819805b251615f7b33
Marc Dominik Migge <marcmigge@gmx.net>
2017-10-12 15:21:05 +0200

    Serve only folder public as static files

--- app/backend/index.js
+++ app/backend/index.js
@@ -4,6 +4,6 @@ let api = require('./api');
 let app = express();
  
 app.use('/api/items', api);
-app.use(express.static('../frontend'));
+app.use(express.static('../frontend/public'));
  
 app.listen(3000);
